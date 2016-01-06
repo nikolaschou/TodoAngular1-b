638bf97282c466c24dad8c8ccb39d7676c8481de
Nikola <nikolaschou@gmail.com>
2016-01-06 15:23:53 +0100

    Adding deployment file

---
+++ deploy.cmd
@@ -0,0 +1,15 @@
+dir
+cd
+
+echo Installing lodash
+call  npm install lodash
+echo Installing node modules
+call npm install -g grunt-cli yo bower
+echo Running gulp
+call grunt build
+echo %DEPLOYMENT_SOURCE% and %DEPLOYMENT_TARGET%
+dir dist
+xcopy %DEPLOYMENT_SOURCE%/dist %DEPLOYMENT_TARGET% /Y
+dir %DEPLOYMENT_TARGET%
+
+
