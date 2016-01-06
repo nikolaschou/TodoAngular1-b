2ad7026ad870280252ec81e8aa35b328574aa85b
Nikola <nikolaschou@gmail.com>
2016-01-06 15:33:31 +0100

    Remove installation of yo from deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -4,7 +4,7 @@ cd
 echo Installing lodash
 call  npm install lodash
 echo Installing node modules
-call npm install -g grunt-cli yo bower
+call npm install -g grunt-cli bower
 echo Running gulp
 call grunt build
 echo %DEPLOYMENT_SOURCE% and %DEPLOYMENT_TARGET%
